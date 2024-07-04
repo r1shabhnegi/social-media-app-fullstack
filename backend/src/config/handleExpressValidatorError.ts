5a2b0655ca0032a5b803fe5a26188260fc1081ab
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-04 17:45:50 +0530

    tsConfig

--- backend/src/config/handleExpressValidatorError.ts
+++ backend/src/config/handleExpressValidatorError.ts
@@ -1,12 +1,13 @@
-import { NextFunction, Request, Response } from 'express';
-import { validationResult } from 'express-validator';
-import { EXPRESS_VALIDATION } from '../utility/errorConstants';
-import { ApiError } from '../utility/apiError';
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { EXPRESS_VALIDATION } from "../utility/errorConstants";
+import { ApiError } from "../utility/apiError";
+// import {} from ""
 
 export const handleValidation = (req: Request) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    let errs = '';
+    let errs = "";
     for (let i = 0; i < errors.array().length; i++) {
       errs += `<- ${errors.array()[i].msg} -> `;
     }
