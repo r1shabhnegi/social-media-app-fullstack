76adebb6eac13a70429b86d81fb825c21163694c
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-13 16:44:33 +0530

    deployment errors fixes14

--- frontend/src/Layout/HomeLayout.tsx
+++ frontend/src/Layout/HomeLayout.tsx
@@ -49,37 +49,39 @@ const HomeLayout = () => {
 
         {/* <CommonLoader isLoading={postsLoading} /> */}
       </div>
-      <div className='sticky hidden lg:flex gap-4 flex-col mr-3 p-4 bg-[#162226] rounded-lg md:block top-20 h-min md:w-60 lg:w-72 xl:w-80'>
-        <div
-          className='bg-[#131A1D] flex cursor-pointer rounded-lg hover:bg-[#1a282d] py-4 items-center gap-4 text-sm px-5'
-          onClick={() => setOpenCreateCom(!openCreateCom)}>
-          <IoAdd className='size-6' />
-          <p>Create a community</p>
-        </div>
-        <div
-          className='bg-[#131A1D] flex cursor-pointer rounded-lg hover:bg-[#1a282d] py-4 items-center gap-4 text-sm px-5'
-          onClick={() => navigate("/submit")}>
-          <IoAdd className='size-6' />
-          <p>Create a Post</p>
-        </div>
-        <div className='bg-[#131A1D] flex-col flex rounded-lg  py-1.5 gap-6 text-sm px-3 '>
-          <h2 className='font-semibold'>Recent Posts</h2>
-          {recentPosts?.map((post: postDataType) => (
-            <div
-              key={post._id}
-              // onClick={}
-              className='flex hover:bg-[#1a282d] px-1.5 py-2 rounded-md cursor-pointer items-center justify-start w-full gap-5'>
-              {/* <span> */}
-              <Avatar className='size-8 sm:size-9'>
-                <AvatarImage src={post.authorAvatar} />
-                <AvatarFallback className='bg-gray-700'>
-                  {post.authorName.slice(0, 2)}
-                </AvatarFallback>
-              </Avatar>
-              <p className='font-semibold line-clamp-2'>{post.title}</p>
-              {/* </span> */}
-            </div>
-          ))}
+      <div className=''>
+        <div className=' sticky hidden lg:flex gap-4 flex-col mr-3 p-4 bg-[#162226] rounded-lg md:block top-20 h-min max-h-[40rem] md:w-60 lg:w-72 xl:w-80'>
+          <div
+            className='bg-[#131A1D] flex cursor-pointer rounded-lg hover:bg-[#1a282d] py-4 items-center gap-4 text-sm px-5'
+            onClick={() => setOpenCreateCom(!openCreateCom)}>
+            <IoAdd className='size-6' />
+            <p>Create a community</p>
+          </div>
+          <div
+            className='bg-[#131A1D] flex cursor-pointer rounded-lg hover:bg-[#1a282d] py-4 items-center gap-4 text-sm px-5'
+            onClick={() => navigate("/submit")}>
+            <IoAdd className='size-6' />
+            <p>Create a Post</p>
+          </div>
+          <div className='overflow-y-auto bg-[#131A1D] flex-col flex rounded-lg  py-1.5 gap-6 text-sm px-3 '>
+            <h2 className='font-semibold'>Recent Posts</h2>
+            {recentPosts?.map((post: postDataType) => (
+              <div
+                key={post._id}
+                // onClick={}
+                className='flex hover:bg-[#1a282d] px-1.5 py-2 rounded-md cursor-pointer items-center justify-start w-full gap-5'>
+                {/* <span> */}
+                <Avatar className='size-8 sm:size-9'>
+                  <AvatarImage src={post.authorAvatar} />
+                  <AvatarFallback className='bg-gray-700'>
+                    {post.authorName.slice(0, 2)}
+                  </AvatarFallback>
+                </Avatar>
+                <p className='font-semibold line-clamp-2'>{post.title}</p>
+                {/* </span> */}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       {openCreateCom && (
