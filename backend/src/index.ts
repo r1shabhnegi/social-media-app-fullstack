740c88882b8a148503eec499387fd00cd0900a1e
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-15 19:43:18 +0530

    deployment errors fixes18

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,11 +25,9 @@ app.use(credentials);
 
 app.use(
   cors({
-    origin: ["https://circlesss.onrender.com", "http://localhost:5173"],
-    credentials: true,
+    origin: "https://circlesss.onrender.com",
   })
 );
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
--- backend/src/middlewares/credentials.middleware.ts
+++ backend/src/middlewares/credentials.middleware.ts
@@ -6,14 +6,23 @@ export const credentials = (
   res: Response,
   next: NextFunction
 ) => {
-  const origin = req.headers.origin;
+  // const origin = req.headers.origin;
   // console.log(req.url);
   // console.log(origin);
-  if (
-    origin === "https://circlesss.onrender.com" ||
-    origin === "circlesss.onrender.com"
-  ) {
-    res.header("Access-Control-Allow-Credentials", "true");
-  }
+  // if (
+  //   origin === "https://circlesss.onrender.com" ||
+  //   origin === "circlesss.onrender.com"
+  // ) {
+  // res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", "https://circlesss.onrender.com");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS, PATCH"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  // }
   next();
 };
