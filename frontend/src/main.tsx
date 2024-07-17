84638f53b1743e8ef4c26097d290c68b26928385
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-17 17:02:55 +0530

    react router dom browserRouter to HashRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { Provider } from 'react-redux';
-import { store } from './global/_store.ts';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./global/_store.ts";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
